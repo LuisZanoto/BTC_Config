5a7583265fda384d73ca9532a5c5207c6375db8c
LUIS LUCA <luiszanoto@yahoo.com>
2022-04-29 14:56:53 -0300

    sec

--- WinBTC/Form1.Designer.cs
+++ WinBTC/Form1.Designer.cs
@@ -30,28 +30,73 @@ namespace WinBTC
         private void InitializeComponent()
         {
             this.Btn_Arq = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtArq = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Btn_Arq
             // 
-            this.Btn_Arq.Location = new System.Drawing.Point(99, 194);
+            this.Btn_Arq.Location = new System.Drawing.Point(19, 37);
             this.Btn_Arq.Name = "Btn_Arq";
-            this.Btn_Arq.Size = new System.Drawing.Size(79, 29);
+            this.Btn_Arq.Size = new System.Drawing.Size(79, 37);
             this.Btn_Arq.TabIndex = 0;
             this.Btn_Arq.Text = "Arquivo";
             this.Btn_Arq.UseVisualStyleBackColor = true;
             this.Btn_Arq.Click += new System.EventHandler(this.Btn_Arq_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtArq);
+            this.groupBox1.Controls.Add(this.Btn_Arq);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(455, 231);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuração :";
+            // 
+            // txtArq
+            // 
+            this.txtArq.Location = new System.Drawing.Point(104, 30);
+            this.txtArq.Name = "txtArq";
+            this.txtArq.Size = new System.Drawing.Size(333, 58);
+            this.txtArq.TabIndex = 1;
+            this.txtArq.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Valor inicial :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(105, 29);
+            this.textBox1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackColor = System.Drawing.Color.Teal;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.Btn_Arq);
+            this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -59,6 +104,11 @@ namespace WinBTC
         #endregion
 
         private System.Windows.Forms.Button Btn_Arq;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox txtArq;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WinBTC/Form1.cs
+++ WinBTC/Form1.cs
@@ -19,7 +19,28 @@ namespace WinBTC
 
         private void Btn_Arq_Click(object sender, EventArgs e)
         {
+            //var fileContent = string.Empty;
+            var filePath = string.Empty;
 
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+
+                openFileDialog.InitialDirectory = @"C:\";
+                openFileDialog.Filter = "xlsx files (*.*) | *.csv";
+                openFileDialog.FilterIndex = 1;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                }
+            }
+
+            txtArq.Text = filePath;
+            //Negocio.ClassGlobal.ArqRede = filePath;
         }
+
+
     }
 }
