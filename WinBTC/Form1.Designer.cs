a6e77329b5c63a8ed78f45b17e6c77c3194126e3
LUIS LUCA <luiszanoto@yahoo.com>
2022-04-29 14:38:27 -0300

    primeiro

---
+++ WinBTC/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace WinBTC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_Arq = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn_Arq
+            // 
+            this.Btn_Arq.Location = new System.Drawing.Point(99, 194);
+            this.Btn_Arq.Name = "Btn_Arq";
+            this.Btn_Arq.Size = new System.Drawing.Size(79, 29);
+            this.Btn_Arq.TabIndex = 0;
+            this.Btn_Arq.Text = "Arquivo";
+            this.Btn_Arq.UseVisualStyleBackColor = true;
+            this.Btn_Arq.Click += new System.EventHandler(this.Btn_Arq_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_Arq);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_Arq;
+    }
+}
+
